41018bb436b9496a554aaa5c9eb4bbabcc9f329d
tomabraham <tomabraham@microsoft.com>
2024-03-20 14:07:06 -0700

    use grpc error code

--- policy/policy.go
+++ policy/policy.go
@@ -13,6 +13,7 @@ import (
 	armPolicy "github.com/Azure/azure-sdk-for-go/sdk/azcore/arm/policy"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/policy"
 	azcorePolicy "github.com/Azure/azure-sdk-for-go/sdk/azcore/policy"
+	"google.golang.org/grpc/codes"
 )
 
 type LoggingPolicy struct {
@@ -24,26 +25,36 @@ func NewLoggingPolicy(logger log.Logger) *LoggingPolicy {
 }
 
 func (p *LoggingPolicy) Do(req *azcorePolicy.Request) (*http.Response, error) {
+	startTime := time.Now()
+	resp, err := req.Next()
+
 	method := req.Raw().Method
 	base := strings.Split(path.Base(req.Raw().URL.String()), "?")
 	methodInfo := method + " " + base[0]
 	parsedURL, parseErr := url.Parse(req.Raw().URL.String())
 	if parseErr != nil {
-		p.logger.Error("Error parsing request URL: ", parseErr)
+		p.logger.With(
+			"grpc.component", "client",
+			"grpc.method", methodInfo,
+			"grpc.service", parsedURL.Host,
+		).Error("Error parsing request URL: ", parseErr)
 		return nil, parseErr
 	}
-	startTime := time.Now()
-	resp, err := req.Next()
 	if err != nil {
-		p.logger.Error("Error finishing request: ", err)
+		p.logger.With(
+			"grpc.component", "client",
+			"grpc.method", methodInfo,
+			"grpc.service", parsedURL.Host,
+		).Error("Error finishing request: ", err)
 		return nil, err
 	}
 
 	// Time is in ms
 	latency := time.Since(startTime).Milliseconds()
 
+	grpcCode := p.ConvertHTTPStatusToGRPCError(resp.StatusCode)
 	p.logger.With(
-		"grpc.code", resp.StatusCode,
+		"grpc.code", grpcCode.String(),
 		"grpc.component", "client",
 		"grpc.time_ms", latency,
 		"grpc.method", methodInfo,
@@ -51,6 +62,35 @@ func (p *LoggingPolicy) Do(req *azcorePolicy.Request) (*http.Response, error) {
 	).Info("finished call")
 
 	return resp, err
+}
+
+func (p *LoggingPolicy) ConvertHTTPStatusToGRPCError(httpStatusCode int) codes.Code {
+	var code codes.Code
+
+	switch httpStatusCode {
+	case http.StatusOK, http.StatusCreated, http.StatusAccepted:
+		code = codes.OK
+	case http.StatusBadRequest:
+		code = codes.InvalidArgument
+	case http.StatusUnauthorized:
+		code = codes.Unauthenticated
+	case http.StatusForbidden:
+		code = codes.PermissionDenied
+	case http.StatusNotFound:
+		code = codes.NotFound
+	case http.StatusTooManyRequests:
+		code = codes.ResourceExhausted
+	case http.StatusInternalServerError:
+		code = codes.Internal
+	case http.StatusNotImplemented:
+		code = codes.Unimplemented
+	case http.StatusServiceUnavailable:
+		code = codes.Unavailable
+	default:
+		code = codes.Unknown
+	}
+
+	return code
 }
 
 func (p *LoggingPolicy) Clone() azcorePolicy.Policy {
