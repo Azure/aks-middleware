703569ac187acaadb1fd63ea50674546afeaff07
tomabraham <tomabraham@microsoft.com>
2024-05-28 10:24:51 -0700

    refactor to ginkgo test

--- httpmw/httpmw_test.go
+++ httpmw/httpmw_test.go
@@ -1,65 +1,73 @@
 package httpmw
 
 import (
+	"bytes"
 	"fmt"
 	"log/slog"
 	"net/http"
 	"net/http/httptest"
 	"os"
-	"testing"
 	"time"
 
 	"github.com/Azure/aks-middleware/ctxlogger"
 	"github.com/gorilla/mux"
+	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	"github.com/stretchr/testify/assert"
 )
 
-func TestPanicMiddleware(t *testing.T) {
-	t.Parallel()
-	g := NewWithT(t)
-	router := mux.NewRouter()
-	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
-	router.Use(NewPanicHandling(logger, customPanicHandler))
-	router.HandleFunc("/", func(w http.ResponseWriter, e *http.Request) {
-		panic("oops")
+var _ = Describe("Httpmw", func() {
+	var (
+		router *mux.Router
+	)
+
+	BeforeEach(func() {
+		router = mux.NewRouter()
 	})
 
-	rw := httptest.NewRecorder()
-	req := httptest.NewRequest("GET", "/", nil)
+	Describe("PanicMiddleware", func() {
+		It("should handle panic and return Internal Server Error", func() {
+			logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+			router.Use(NewPanicHandling(logger, customPanicHandler))
+			router.HandleFunc("/", func(w http.ResponseWriter, e *http.Request) {
+				panic("oops")
+			})
 
-	router.ServeHTTP(rw, req)
+			rw := httptest.NewRecorder()
+			req := httptest.NewRequest("GET", "/", nil)
 
-	g.Expect(rw.Body.String()).To(ContainSubstring("Internal Server Error"))
-	g.Expect(rw.Result().StatusCode).To(Equal(500))
-}
+			router.ServeHTTP(rw, req)
+
+			Expect(rw.Body.String()).To(ContainSubstring("Internal Server Error"))
+			Expect(rw.Result().StatusCode).To(Equal(500))
+		})
+	})
+
+	Describe("LoggingMiddleware", func() {
+		It("should log and return OK status", func() {
+			router.Use(NewLogging())
+
+			router.HandleFunc("/", func(w http.ResponseWriter, e *http.Request) {
+				time.Sleep(10 * time.Millisecond)
+				w.WriteHeader(http.StatusOK)
+			})
+
+			rw := httptest.NewRecorder()
+			req := httptest.NewRequest("GET", "/", nil)
+			var buf bytes.Buffer
+
+			// attaching logger to ctx
+			router.ServeHTTP(rw, req.WithContext(ctxlogger.WithLogger(req.Context(), slog.New(slog.NewJSONHandler(&buf, nil)))))
+
+			Expect(buf.String()).To(ContainSubstring("finished call"))
+			Expect(rw.Result().StatusCode).To(Equal(200))
+
+		})
+	})
+})
 
 // Custom panic handler for testing
 func customPanicHandler(logger *slog.Logger, w http.ResponseWriter, r *http.Request, err interface{}) {
 	logger.Info(fmt.Sprintf("Custom panic occurred: %v", err))
 	// Additional custom handling logic here
 	http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-}
-
-func TestLoggingMiddleware(t *testing.T) {
-	t.Parallel()
-	g := NewWithT(t)
-	router := mux.NewRouter()
-	router.Use(NewLogging())
-
-	router.HandleFunc("/", func(w http.ResponseWriter, e *http.Request) {
-		time.Sleep(10 * time.Millisecond)
-		w.WriteHeader(http.StatusOK)
-	})
-
-	req := httptest.NewRequest("GET", "/", nil)
-	w := httptest.NewRecorder()
-
-	router.ServeHTTP(w, req.WithContext(ctxlogger.WithLogger(req.Context(), slog.New(slog.NewJSONHandler(os.Stdout, nil)))))
-	resp := w.Result()
-
-	assert.Equal(t, http.StatusOK, resp.StatusCode)
-	g.Expect(w.Body.String()).To(ContainSubstring("Finished call"))
-
-	defer resp.Body.Close()
 }
