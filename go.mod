42e21753140801470846e3aaf3ecdd4303648ed9
tomabraham <tomabraham@microsoft.com>
2024-10-07 12:05:08 -0700

    adding otel audit mw + tests

--- go.mod
+++ go.mod
@@ -12,30 +12,38 @@ require (
 	github.com/gofrs/uuid v4.4.0+incompatible
 	github.com/gorilla/mux v1.8.1
 	github.com/grpc-ecosystem/go-grpc-middleware/v2 v2.0.1
-	github.com/grpc-ecosystem/grpc-gateway/v2 v2.24.0
+	github.com/microsoft/go-otel-audit v0.2.0
 	github.com/onsi/ginkgo/v2 v2.13.2
 	github.com/onsi/gomega v1.30.0
-	google.golang.org/genproto/googleapis/api v0.0.0-20241118233622-e639e219e697
-	google.golang.org/grpc v1.68.0
-	google.golang.org/protobuf v1.35.2
+	google.golang.org/grpc v1.62.1
+	google.golang.org/protobuf v1.33.0
 )
 
 require (
 	github.com/Azure/azure-sdk-for-go/sdk/internal v1.5.1 // indirect
+	github.com/Azure/retry v0.0.0-20240325164105-70e16f388626 // indirect
 	github.com/antlr4-go/antlr/v4 v4.13.0 // indirect
+	github.com/go-json-experiment/json v0.0.0-20240418180308-af2d5061e6c2 // indirect
 	github.com/go-logr/logr v1.3.0 // indirect
 	github.com/go-task/slim-sprig v0.0.0-20230315185526-52ccab3ef572 // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/google/cel-go v0.18.2 // indirect
 	github.com/google/go-cmp v0.6.0 // indirect
-	github.com/google/pprof v0.0.0-20210407192527-94a9f03dee38 // indirect
-	github.com/rogpeppe/go-internal v1.13.1 // indirect
+	github.com/google/pprof v0.0.0-20211214055906-6f57359322fd // indirect
+	github.com/jedib0t/go-pretty/v6 v6.5.6 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/sanity-io/litter v1.5.5 // indirect
 	github.com/stoewer/go-strcase v1.3.0 // indirect
-	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
-	golang.org/x/net v0.29.0 // indirect
-	golang.org/x/sys v0.25.0 // indirect
-	golang.org/x/text v0.20.0 // indirect
-	golang.org/x/tools v0.22.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20241118233622-e639e219e697 // indirect
+	github.com/vmihailenco/msgpack/v4 v4.3.13 // indirect
+	github.com/vmihailenco/tagparser v0.1.1 // indirect
+	golang.org/x/exp v0.0.0-20240416160154-fe59bbe5cc7f // indirect
+	golang.org/x/net v0.28.0 // indirect
+	golang.org/x/sys v0.23.0 // indirect
+	golang.org/x/text v0.17.0 // indirect
+	golang.org/x/tools v0.21.1-0.20240508182429-e35e4ccd0d2d // indirect
+	google.golang.org/appengine v1.6.8 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20240123012728-ef4313101c80 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240318140521-94a12d6c2237 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- http/server/logging/logging.go
+++ http/server/logging/logging.go
@@ -2,24 +2,30 @@ package logging
 
 import (
 	"context"
+	"fmt"
 	"log/slog"
+	"net"
 	"net/http"
 	"time"
 
 	"github.com/gorilla/mux"
 	"google.golang.org/grpc/metadata"
+	"github.com/microsoft/go-otel-audit/audit"
+	"github.com/microsoft/go-otel-audit/audit/msgs"
 )
 
 // TODO (Tom): Add a logger wrapper in its own package
 // https://medium.com/@ansujain/building-a-logger-wrapper-in-go-with-support-for-multiple-logging-libraries-48092b826bee
 
 // more info about http handler here: https://pkg.go.dev/net/http#Handler
-func NewLogging(logger *slog.Logger) mux.MiddlewareFunc {
+func NewLogging(logger *slog.Logger, otelAuditClient *audit.Client, customOperationDescriptions map[string]string) mux.MiddlewareFunc {
 	return func(next http.Handler) http.Handler {
 		return &loggingMiddleware{
-			next:   next,
-			now:    time.Now,
-			logger: *logger,
+			next:                                  next,
+			now:                                   time.Now,
+			logger:                                *logger,
+			otelAuditClient:                       otelAuditClient,
+			operationCategoryDescriptionsForOther: customOperationDescriptions,
 		}
 	}
 }
@@ -28,9 +34,11 @@ func NewLogging(logger *slog.Logger) mux.MiddlewareFunc {
 var _ http.Handler = &loggingMiddleware{}
 
 type loggingMiddleware struct {
-	next   http.Handler
-	now    func() time.Time
-	logger slog.Logger
+	next                                  http.Handler
+	now                                   func() time.Time
+	logger                                slog.Logger
+	otelAuditClient                       *audit.Client
+	operationCategoryDescriptionsForOther map[string]string
 }
 
 type responseWriter struct {
@@ -99,4 +107,119 @@ func (l *loggingMiddleware) LogRequestStart(ctx context.Context, r *http.Request
 func (l *loggingMiddleware) LogRequestEnd(ctx context.Context, r *http.Request, msg string, statusCode int, duration time.Duration) {
 	attributes := BuildAttributes(ctx, r, "code", statusCode, "time_ms", duration.Milliseconds())
 	l.logger.InfoContext(ctx, msg, attributes...)
+}
+
+func (l *loggingMiddleware) sendOtelAuditEvent(ctx context.Context, statusCode int, req *http.Request) {
+	if l.otelAuditClient == nil {
+		l.logger.Error("otel audit client is nil")
+		return
+	}
+
+	msg := l.createOtelAuditEvent(ctx, statusCode, req)
+	l.logger.Info("sending audit logs")
+	if err := l.otelAuditClient.Send(ctx, msg); err != nil {
+		l.logger.Error("failed to send audit event", "error", err)
+	} else {
+		l.logger.Info("audit event sent successfully")
+	}
+}
+
+func (l *loggingMiddleware) createOtelAuditEvent(ctx context.Context, statusCode int, req *http.Request) msgs.Msg {
+	host, _, err := net.SplitHostPort(req.RemoteAddr)
+	if err != nil {
+		l.logger.Error("failed to split host and port", "error", err)
+	}
+	addr, err := msgs.ParseAddr(host)
+	if err != nil {
+		l.logger.Error("failed to parse address", "error", err)
+	}
+
+	tr := map[string][]msgs.TargetResourceEntry{
+		"ResourceType": {
+			{
+				Name:   req.RequestURI,
+				Region: req.Header.Get("Region"), // Assume the region is in the header
+			},
+		},
+	}
+
+	record := msgs.Record{
+		CallerIpAddress:              addr,
+		CallerIdentities:             getCallerIdentities(req), // No OperationTracking needed
+		OperationCategories:          []msgs.OperationCategory{l.getOperationCategory(req.Method)},
+		OperationCategoryDescription: l.getOperationCategoryDescription(req.Method),
+		TargetResources:              tr,
+		CallerAccessLevels:           []string{"NA"}, // As before, assuming the access level is pre-defined
+		OperationAccessLevel:         "Azure Kubernetes Fleet Manager Contributor Role",
+		OperationName:                req.Method, // Method as operation name
+		CallerAgent:                  req.UserAgent(),
+		OperationType:                getOperationType(req.Method),
+		OperationResult:              getOperationResult(statusCode),
+		OperationResultDescription:   getOperationResultDescription(statusCode),
+	}
+
+	return msgs.Msg{
+		Type:   msgs.ControlPlane,
+		Record: record,
+	}
+}
+
+// Simplified caller identity map creation based on the request
+func getCallerIdentities(req *http.Request) map[msgs.CallerIdentityType][]msgs.CallerIdentityEntry {
+	caller := make(map[msgs.CallerIdentityType][]msgs.CallerIdentityEntry)
+
+	// Here you can parse headers or other request properties to map identities
+	clientAppID := req.Header.Get("ClientAppID")
+	if clientAppID != "" {
+		caller[msgs.ApplicationID] = []msgs.CallerIdentityEntry{
+			{
+				Identity:    clientAppID,
+				Description: "client application ID",
+			},
+		}
+	}
+
+	return caller
+}
+
+// Updated to use HTTP method to determine the operation category
+func (l *loggingMiddleware) getOperationCategory(method string) msgs.OperationCategory {
+	if _, ok := l.operationCategoryDescriptionsForOther[method]; ok {
+		return msgs.OCOther
+	}
+	return msgs.ResourceManagement
+}
+
+func (l *loggingMiddleware) getOperationCategoryDescription(method string) string {
+	if desc, ok := l.operationCategoryDescriptionsForOther[method]; ok {
+		return desc
+	}
+	return ""
+}
+
+func getOperationType(method string) msgs.OperationType {
+	switch method {
+	case http.MethodGet:
+		return msgs.Read
+	case http.MethodPatch, http.MethodPost, http.MethodPut:
+		return msgs.Update
+	case http.MethodDelete:
+		return msgs.Delete
+	default:
+		return msgs.Read
+	}
+}
+
+func getOperationResult(statusCode int) msgs.OperationResult {
+	if statusCode >= 400 {
+		return msgs.Failure
+	}
+	return msgs.Success
+}
+
+func getOperationResultDescription(statusCode int) string {
+	if statusCode >= 400 {
+		return fmt.Sprintf("operation failed with status code: %d", statusCode)
+	}
+	return "succeeded to run the operation"
 }
