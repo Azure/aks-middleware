673341c63fe70ca99b5051741ed26776d5b48604
tomabraham <tomabraham@microsoft.com>
2024-12-19 11:43:18 -0800

    update metadata test

--- httpmw/metadata/metadata_test.go
+++ httpmw/metadata/metadata_test.go
@@ -9,35 +9,51 @@ import (
 
 var _ = Describe("Metadata", func() {
 	Describe("extractMetadata", func() {
-		It("should extract metadata from HTTP headers", func() {
-			headersToMetadata := map[string]string{
+		var (
+			headersToMetadata map[string]string
+			req               *http.Request
+			err               error
+		)
+
+		BeforeEach(func() {
+			headersToMetadata = map[string]string{
 				"X-Custom-Header": "custom-header",
 			}
-			req, err := http.NewRequest("GET", "http://example.com", nil)
+			req, err = http.NewRequest("GET", "http://example.com", nil)
 			Expect(err).NotTo(HaveOccurred())
+		})
+
+		It("should extract metadata from HTTP headers", func() {
 			req.Header.Add("X-Custom-Header", "value")
 
 			md := extractMetadata(headersToMetadata, req)
 			Expect(md["custom-header"]).To(ContainElement("value"))
 		})
+
+		It("should ignore headers not in the headersToMetadata map", func() {
+			req.Header.Add("X-Irrelevant-Header", "value")
+
+			md := extractMetadata(headersToMetadata, req)
+			Expect(md).NotTo(HaveKey("irrelevant-header"))
+		})
 	})
 
 	Describe("matchOutgoingHeader", func() {
-		It("should match allowed headers", func() {
-			allowedHeaders := map[string]string{
+		var allowedHeaders map[string]string
+
+		BeforeEach(func() {
+			allowedHeaders = map[string]string{
 				"custom-header": "X-Custom-Header",
 			}
+		})
 
+		It("should match allowed headers", func() {
 			header, ok := matchOutgoingHeader(allowedHeaders, "custom-header")
 			Expect(ok).To(BeTrue())
 			Expect(header).To(Equal("X-Custom-Header"))
 		})
 
 		It("should not match disallowed headers", func() {
-			allowedHeaders := map[string]string{
-				"custom-header": "X-Custom-Header",
-			}
-
 			header, ok := matchOutgoingHeader(allowedHeaders, "other-header")
 			Expect(ok).To(BeFalse())
 			Expect(header).To(Equal(""))
