5776fd43d0bd0ca1850f0dc07221436ac71745f5
tomabraham <tomabraham@microsoft.com>
2025-04-09 18:03:37 -0700

    convert to spaces

--- http/server/operationrequest/operationrequest.go
+++ http/server/operationrequest/operationrequest.go
@@ -1,13 +1,13 @@
 package operationrequest
 
 import (
-	"context"
-	"fmt"
-	"net/http"
-	"time"
+    "context"
+    "fmt"
+    "net/http"
+    "time"
 
-	"github.com/Azure/aks-middleware/http/common"
-	"github.com/gorilla/mux"
+    "github.com/Azure/aks-middleware/http/common"
+    "github.com/gorilla/mux"
 )
 
 const ARMTimeout = 60 * time.Second
@@ -17,33 +17,33 @@ var _ http.Handler = &operationRequestMiddleware{}
 // NewOperationRequest creates an operationRequestMiddleware using the provided options.
 // The options contains both the Extras value and its customizer
 func NewOperationRequest(region string, opts OperationRequestOptions) mux.MiddlewareFunc {
-	return func(next http.Handler) http.Handler {
-		return &operationRequestMiddleware{
-			next:   next,
-			region: region,
-			opts:   opts,
-		}
-	}
+    return func(next http.Handler) http.Handler {
+        return &operationRequestMiddleware{
+            next:   next,
+            region: region,
+            opts:   opts,
+        }
+    }
 }
 
 type operationRequestMiddleware struct {
-	next   http.Handler
-	region string
-	opts   OperationRequestOptions
+    next   http.Handler
+    region string
+    opts   OperationRequestOptions
 }
 
 func (op *operationRequestMiddleware) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	opReq, err := NewBaseOperationRequest(r, op.region, op.opts)
-	if err != nil {
-		http.Error(w, fmt.Errorf("failed to create operation request: %w", err).Error(), http.StatusInternalServerError)
-		return
-	}
+    opReq, err := NewBaseOperationRequest(r, op.region, op.opts)
+    if err != nil {
+        http.Error(w, fmt.Errorf("failed to create operation request: %w", err).Error(), http.StatusInternalServerError)
+        return
+    }
 
-	ctx := r.Context()
-	ctx = OperationRequestWithContext(ctx, opReq)
-	ctx, cancel := context.WithTimeout(ctx, ARMTimeout)
-	defer cancel()
-	enrichedReq := r.WithContext(ctx)
-	enrichedReq.Header.Set(common.RequestAcsOperationIDHeader, opReq.OperationID)
-	op.next.ServeHTTP(w, enrichedReq)
+    ctx := r.Context()
+    ctx = OperationRequestWithContext(ctx, opReq)
+    ctx, cancel := context.WithTimeout(ctx, ARMTimeout)
+    defer cancel()
+    enrichedReq := r.WithContext(ctx)
+    enrichedReq.Header.Set(common.RequestAcsOperationIDHeader, opReq.OperationID)
+    op.next.ServeHTTP(w, enrichedReq)
 }
