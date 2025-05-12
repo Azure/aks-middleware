bb47a59a8faa4165e51350b794c628a8030c369d
manasachi <manasachinta7@gmail.com>
2025-05-12 01:39:18 +0000

    makes a diff?

--- http/common/const.go
+++ http/common/const.go
@@ -1,42 +1,42 @@
 package common
 
 const (
-    CorrelationIDKey      = "correlationid"
-    OperationIDKey        = "operationid"
-    ARMClientRequestIDKey = "armclientrequestid"
-    RequestIDLogKey       = "request-id"
+	CorrelationIDKey      = "correlationid"
+	OperationIDKey        = "operationid"
+	ARMClientRequestIDKey = "armclientrequestid"
+	RequestIDLogKey       = "request-id"
 
-    // Details can be found here:
-    // https://github.com/Azure/azure-resource-manager-rpc/blob/master/v1.0/common-api-details.md#client-request-headers
-    RequestCorrelationIDHeader = "x-ms-correlation-request-id"
-    // RequestAcsOperationIDHeader is the http header name ACS RP adds for operation ID (AKS specific)
-    RequestAcsOperationIDHeader = "x-ms-acs-operation-id"
-    // RequestARMClientRequestIDHeader  Caller-specified value identifying the request, in the form of a GUID
-    RequestARMClientRequestIDHeader = "x-ms-client-request-id"
-    // RequestIDMetadataKey is the key in the gRPC
-    // metadata.
-    RequestIDMetadataHeader = "x-request-id"
+	// Details can be found here:
+	// https://github.com/Azure/azure-resource-manager-rpc/blob/master/v1.0/common-api-details.md#client-request-headers
+	RequestCorrelationIDHeader = "x-ms-correlation-request-id"
+	// RequestAcsOperationIDHeader is the http header name ACS RP adds for operation ID (AKS specific)
+	RequestAcsOperationIDHeader = "x-ms-acs-operation-id"
+	// RequestARMClientRequestIDHeader  Caller-specified value identifying the request, in the form of a GUID
+	RequestARMClientRequestIDHeader = "x-ms-client-request-id"
+	// RequestIDMetadataKey is the key in the gRPC
+	// metadata.
+	RequestIDMetadataHeader     = "x-request-id"
 	RequestAcceptLanguageHeader = "Accept-Language"
 )
 
 const (
-    SubscriptionIDKey   = "subscriptionId"
-    ResourceGroupKey    = "resourceGroup"
-    APIVersionKey       = "api-version"
-    ResourceProviderKey = "resourceProvider"
-    ResourceTypeKey     = "resourceType"
-    ResourceNameKey     = "resourceName"
+	SubscriptionIDKey   = "subscriptionID"
+	ResourceGroupKey    = "resourceGroup"
+	APIVersionKey       = "api-version"
+	ResourceProviderKey = "resourceProvider"
+	ResourceTypeKey     = "resourceType"
+	ResourceNameKey     = "resourceName"
 )
 
 var (
-    MetadataToHeader = map[string]string{
-        OperationIDKey:        RequestAcsOperationIDHeader,
-        ARMClientRequestIDKey: RequestARMClientRequestIDHeader,
-    }
+	MetadataToHeader = map[string]string{
+		OperationIDKey:        RequestAcsOperationIDHeader,
+		ARMClientRequestIDKey: RequestARMClientRequestIDHeader,
+	}
 
-    HeaderToMetadata = map[string]string{
-        RequestCorrelationIDHeader:     CorrelationIDKey,
-        RequestAcsOperationIDHeader:    OperationIDKey,
-        RequestARMClientRequestIDHeader: ARMClientRequestIDKey,
-    }
+	HeaderToMetadata = map[string]string{
+		RequestCorrelationIDHeader:      CorrelationIDKey,
+		RequestAcsOperationIDHeader:     OperationIDKey,
+		RequestARMClientRequestIDHeader: ARMClientRequestIDKey,
+	}
 )
