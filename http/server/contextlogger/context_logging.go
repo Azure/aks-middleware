290d08dace373ad6d29a2ff6101aa25ecbeaf7e5
manasachi <manasachinta7@gmail.com>
2025-06-12 18:54:31 +0000

    update ctxlogging tests

--- http/server/contextlogger/context_logging.go
+++ http/server/contextlogger/context_logging.go
@@ -1,40 +1,41 @@
 package contextlogger
 
 import (
-    "context"
-    "encoding/json"
-    log "log/slog"
-    "net/http"
-    "os"
+	"context"
+	"encoding/json"
+	"fmt"
+	log "log/slog"
+	"net/http"
+	"os"
 
-    opreq "github.com/Azure/aks-middleware/http/server/operationrequest"
-    "github.com/gorilla/mux"
-    "google.golang.org/grpc/metadata"
+	opreq "github.com/Azure/aks-middleware/http/server/operationrequest"
+	"github.com/gorilla/mux"
+	"google.golang.org/grpc/metadata"
 )
 
 type ExtractFunction func(ctx context.Context, r *http.Request) map[string]interface{}
 
 const (
-    ctxLogSource = "CtxLog"
+	ctxLogSource = "CtxLog"
 )
 
 type loggerKeyType int
 
 const (
-    loggerKey loggerKeyType = iota
+	loggerKey loggerKeyType = iota
 )
 
 // DefaultExtractor extracts operation request fields from the context.
 // It returns the filtered map containing only the specified keys.
 func DefaultExtractor(ctx context.Context, r *http.Request) map[string]interface{} {
-    op := opreq.OperationRequestFromContext(ctx)
-    if op == nil {
-        return nil
-    }
-    return opreq.FilteredOperationRequestMap(op, []string{
-        "TargetURI", "HttpMethod", "AcceptedLanguage", "APIVersion", "Region",
-        "SubscriptionID", "ResourceGroup", "ResourceName", "CorrelationID", "OperationID",
-    })
+	op := opreq.OperationRequestFromContext(ctx)
+	if op == nil {
+		return nil
+	}
+	return opreq.FilteredOperationRequestMap(op, []string{
+		"TargetURI", "HttpMethod", "AcceptedLanguage", "APIVersion", "Region",
+		"SubscriptionID", "ResourceGroup", "ResourceName", "CorrelationID", "OperationID",
+	})
 }
 
 // New creates a context logging middleware.
@@ -43,106 +44,104 @@ func DefaultExtractor(ctx context.Context, r *http.Request) map[string]interface
 //	logger:                  A slog.Logger instance used for logging. Any static attributes added to this logger before passing it in will be preserved
 //	extractFunction:         ExtractFunction extracts information from the ctx and/or the request and put it in the logger
 func New(logger log.Logger, extractFunction ExtractFunction) mux.MiddlewareFunc {
-    if extractFunction == nil {
-        extractFunction = DefaultExtractor
-    }
-    return func(next http.Handler) http.Handler {
-        return &contextLogMiddleware{
-            next:            next,
-            logger:          logger,
-            extractFunction: extractFunction,
-        }
-    }
+	if extractFunction == nil {
+		extractFunction = DefaultExtractor
+	}
+	return func(next http.Handler) http.Handler {
+		return &contextLogMiddleware{
+			next:            next,
+			logger:          logger,
+			extractFunction: extractFunction,
+		}
+	}
 }
 
 // Enforce that contextLogMiddleware implements http.Handler.
 var _ http.Handler = &contextLogMiddleware{}
 
 type contextLogMiddleware struct {
-    next            http.Handler
-    logger          log.Logger
-    extractFunction ExtractFunction
+	next            http.Handler
+	logger          log.Logger
+	extractFunction ExtractFunction
 }
 
 func (m *contextLogMiddleware) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-    ctx := r.Context()
-    attributes := BuildAttributes(ctx, r, m.extractFunction)
+	ctx := r.Context()
+	attributes := BuildAttributes(ctx, r, m.extractFunction)
 
-    contextLogger := m.logger.With(attributes...)
-    ctx = context.WithValue(ctx, loggerKey, contextLogger)
-    r = r.WithContext(ctx)
+	contextLogger := m.logger.With(attributes...)
+	ctx = context.WithValue(ctx, loggerKey, contextLogger)
+	r = r.WithContext(ctx)
 
-    m.next.ServeHTTP(w, r)
+	m.next.ServeHTTP(w, r)
 }
 
 func BuildAttributes(ctx context.Context, r *http.Request, extractFunc func(ctx context.Context, r *http.Request) map[string]interface{}) []interface{} {
-    md, ok := metadata.FromIncomingContext(ctx)
-    headers := make(map[string]string)
-    if ok {
-        for key, values := range md {
-            if len(values) > 0 {
-                headers[key] = values[0]
-            }
-        }
-    }
+	md, ok := metadata.FromIncomingContext(ctx)
+	headers := make(map[string]string)
+	if ok {
+		for key, values := range md {
+			if len(values) > 0 {
+				headers[key] = values[0]
+			}
+		}
+	}
 
-    attributes := defaultCtxLogAttributes(r)
-    logAttrs := make(map[string]interface{})
+	attributes := defaultCtxLogAttributes(r)
+	logAttrs := make(map[string]interface{})
 
-    // Use the extract function to get additional attributes.
-    if extractFunc != nil {
-        extractedAttrs := extractFunc(ctx, r)
-        for k, v := range extractedAttrs {
-            logAttrs[k] = v
-        }
-    }
+	// Use the extract function to get additional attributes.
+	if extractFunc != nil {
+		extractedAttrs := extractFunc(ctx, r)
+		for k, v := range extractedAttrs {
+			logAttrs[k] = v
+		}
+	}
 
-    attrBytes, err := json.Marshal(logAttrs)
-    templogger := log.New(log.NewJSONHandler(os.Stdout, nil)).With("source", ctxLogSource)
-    if err != nil {
-        templogger.Error("error building attributes for additional attributes", "error", err)
-    }
-    attributesStr := string(attrBytes)
+	attrBytes, errMarshalAttrs := json.Marshal(logAttrs)
+	templogger := log.New(log.NewJSONHandler(os.Stdout, nil)).With("source", ctxLogSource)
+	if errMarshalAttrs != nil {
+		templogger.Error("error building attributes for additional attributes", "error", errMarshalAttrs)
+	}
+	attributesStr := string(attrBytes)
 
-    headerBytes, err := json.Marshal(headers)
-    headersStr := string(headerBytes)
+	headerBytes, errMarshalHeaders := json.Marshal(headers)
+	headersStr := string(headerBytes)
+	if errMarshalHeaders != nil {
+		templogger.Error("error building attributes for headers", "error", errMarshalHeaders)
+	}
 
-    var finalHeaders, finalAttributes interface{}
-    if err != nil {
-        templogger.Error("error marshaling headers", "error", err)
-        finalHeaders = headers
-        finalAttributes = logAttrs
-    } else {
-        finalHeaders = headersStr
-        finalAttributes = attributesStr
-    }
+	if errMarshalAttrs != nil || errMarshalHeaders != nil {
+		fmt.Println("log attrs: ", logAttrs)
+		attributes = append(attributes, "headers", headers) // Include metadata headers as part of the attributes.
+		attributes = append(attributes, "log", logAttrs)    // grab desired headers from the request (based on extraction function passed to request ID middleware)
+	} else {
+		attributes = append(attributes, "headers", headersStr)
+		attributes = append(attributes, "log", attributesStr)
+	}
 
-    // Include metadata headers as part of the attributes.
-    attributes = append(attributes, "log", finalAttributes)
-    // grab desired headers from the request (based on extraction function passed to request ID middleware)
-    attributes = append(attributes, "headers", finalHeaders)
-    return attributes
+	return attributes
 }
 
 func defaultCtxLogAttributes(r *http.Request) []interface{} {
-    return []interface{}{
-        "request", r.URL.Path,
-        "method", r.Method,
-        "source", ctxLogSource,
-    }
+	return []interface{}{
+		"request", r.URL.Path,
+		"method", r.Method,
+		"source", ctxLogSource,
+	}
 }
 
 func GetLogger(ctx context.Context) *log.Logger {
-    logger := log.Default().With("src", "self gen, not available in ctx")
-    if ctx == nil {
-        return logger
-    }
-    if ctxlogger, ok := ctx.Value(loggerKey).(*log.Logger); ok {
-        return ctxlogger
-    }
-    return logger
+	logger := log.Default().With("src", "self gen, not available in ctx")
+	if ctx == nil {
+		return logger
+	}
+	if ctxlogger, ok := ctx.Value(loggerKey).(*log.Logger); ok {
+		return ctxlogger
+	}
+	return logger
 }
 
 func WithLogger(ctx context.Context, logger *log.Logger) context.Context {
-    return context.WithValue(ctx, loggerKey, logger)
+	return context.WithValue(ctx, loggerKey, logger)
 }
--- http/server/contextlogger/context_logging_test.go
+++ http/server/contextlogger/context_logging_test.go
@@ -1,230 +1,231 @@
 package contextlogger
 
 import (
-	"bytes"
-	"context"
-	"encoding/json"
-	"fmt"
-	"io"
-	"log/slog"
-	"net/http"
-	"net/http/httptest"
-	"os"
-	"strings"
+    "bytes"
+    "context"
+    "encoding/json"
+    "fmt"
+    "io"
+    "log/slog"
+    "net/http"
+    "net/http/httptest"
+    "os"
+    "strings"
 
-	common "github.com/Azure/aks-middleware/http/common/logging"
-	"github.com/Azure/aks-middleware/http/server/requestid"
-	"github.com/gorilla/mux"
-	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
+     common "github.com/Azure/aks-middleware/http/common/logging"
+    "github.com/Azure/aks-middleware/http/server/requestid"
+    "github.com/gorilla/mux"
+    . "github.com/onsi/ginkgo/v2"
+    . "github.com/onsi/gomega"
 )
 
 type routerConfig struct {
-	buf         *bytes.Buffer
-	logger      *slog.Logger
-	extractFunc func(ctx context.Context, r *http.Request) map[string]interface{}
+    buf         *bytes.Buffer
+    logger      *slog.Logger
+    extractFunc func(ctx context.Context, r *http.Request) map[string]interface{}
 }
 
 // should not be able to marshal this type to a string for logging
 type InvalidType struct {
-	Fn func()
+    Fn func()
 }
 
 const (
-	subscriptionIDKey    = "subscriptionID"
-	resourceGroupNameKey = "resourceGroupName"
-	resultTypeKey        = "resultType"
-	errorDetailsKey      = "errorDetails"
+    subscriptionIDKey    = "subscriptionID"
+    resourceGroupNameKey = "resourceGroupName"
+    resultTypeKey        = "resultType"
+    errorDetailsKey      = "errorDetails"
 
-	defaultRouterName         = "default"
-	extraLoggingVariablesName = "extra"
-	extraLoggingCannotMarshal = "extraCannotMarshal"
-	customTestRouterName      = "custom"
+    defaultRouterName         = "default"
+    extraLoggingVariablesName = "extra"
+    extraLoggingCannotMarshal = "extraCannotMarshal"
+    customTestRouterName      = "custom"
 
-	customTestKey   = "testKey"
-	customTestValue = "testValue"
+    customTestKey   = "testKey"
+    customTestValue = "testValue"
 )
 
 var _ = Describe("HttpmwWithCustomAttributeLogging", Ordered, func() {
-	var (
-		// custom extractor for request IDs used by the request id middleware.
-		requestIDExtractor = func(r *http.Request) map[string]string {
-			return map[string]string{
-				string(requestid.CorrelationIDKey): r.Header.Get(requestid.RequestCorrelationIDHeader),
-				string(requestid.OperationIDKey):   r.Header.Get(requestid.RequestAcsOperationIDHeader),
-			}
-		}
+    var (
+        // custom extractor for request IDs used by the request id middleware.
+        requestIDExtractor = func(r *http.Request) map[string]string {
+            return map[string]string{
+                string(requestid.CorrelationIDKey): r.Header.Get(requestid.RequestCorrelationIDHeader),
+                string(requestid.OperationIDKey):   r.Header.Get(requestid.RequestAcsOperationIDHeader),
+            }
+        }
 
-		// router configurations
-		routerConfigs = map[string]*routerConfig{
-			defaultRouterName: {
-				extractFunc: nil,
-			},
-			extraLoggingVariablesName: {
-				extractFunc: func(ctx context.Context, r *http.Request) map[string]interface{} {
-					attrs := make(map[string]interface{})
-					attrs[subscriptionIDKey] = "extractedSubIDvalue"
-					attrs[resourceGroupNameKey] = "extractedRGnamevalue"
-					attrs[resultTypeKey] = 3
-					attrs[errorDetailsKey] = "extractedErrorDetailsvalue"
-					return attrs
-				},
-			},
-			extraLoggingCannotMarshal: {
-				extractFunc: func(ctx context.Context, r *http.Request) map[string]interface{} {
-					attrs := make(map[string]interface{})
-					attrs[subscriptionIDKey] = "extractedSubIDvalue"
-					attrs[resourceGroupNameKey] = InvalidType{Fn: func() {
-						panic("cannot marshal this value")
-					}}
-					attrs[resultTypeKey] = 3
-					attrs[errorDetailsKey] = "extractedErrorDetailsvalue"
-					return attrs
-				},
-			},
-			customTestRouterName: {
-				extractFunc: nil,
-			},
-		}
+        // router configurations
+        routerConfigs = map[string]*routerConfig{
+            defaultRouterName: {
+                extractFunc: nil,
+            },
+            extraLoggingVariablesName: {
+                extractFunc: func(ctx context.Context, r *http.Request) map[string]interface{} {
+                    attrs := make(map[string]interface{})
+                    attrs[subscriptionIDKey] = "extractedSubIDvalue"
+                    attrs[resourceGroupNameKey] = "extractedRGnamevalue"
+                    attrs[resultTypeKey] = 3
+                    attrs[errorDetailsKey] = "extractedErrorDetailsvalue"
+                    return attrs
+                },
+            },
+            extraLoggingCannotMarshal: {
+                extractFunc: func(ctx context.Context, r *http.Request) map[string]interface{} {
+                    attrs := make(map[string]interface{})
+                    attrs[subscriptionIDKey] = "extractedSubIDvalue"
+                    attrs[resourceGroupNameKey] = InvalidType{Fn: func() {
+                        panic("cannot marshal this value")
+                    }}
+                    attrs[resultTypeKey] = 3
+                    attrs[errorDetailsKey] = "extractedErrorDetailsvalue"
+                    return attrs
+                },
+            },
+            customTestRouterName: {
+                extractFunc: nil,
+            },
+        }
 
-		routersMap = map[string]*mux.Router{}
-	)
+        routersMap = map[string]*mux.Router{}
+    )
 
-	buildRouter := func(cfg *routerConfig) *mux.Router {
-		r := mux.NewRouter()
-		r.Use(requestid.NewRequestIDMiddlewareWithExtractor(requestIDExtractor))
+    buildRouter := func(cfg *routerConfig) *mux.Router {
+        r := mux.NewRouter()
+        r.Use(requestid.NewRequestIDMiddlewareWithExtractor(requestIDExtractor))
 
-		cfg.buf = new(bytes.Buffer)
-		// For customTestRouter, supply the logger with static attributes.
-		if cfg == routerConfigs[customTestRouterName] {
-			cfg.logger = slog.New(slog.NewJSONHandler(cfg.buf, nil)).With(customTestKey, customTestValue)
-		} else {
-			cfg.logger = slog.New(slog.NewJSONHandler(cfg.buf, nil))
-		}
+        cfg.buf = new(bytes.Buffer)
+        // For customTestRouter, supply the logger with static attributes.
+        if cfg == routerConfigs[customTestRouterName] {
+            cfg.logger = slog.New(slog.NewJSONHandler(cfg.buf, nil)).With(customTestKey, customTestValue)
+        } else {
+            cfg.logger = slog.New(slog.NewJSONHandler(cfg.buf, nil))
+        }
 
-		r.Use(New(*cfg.logger, cfg.extractFunc))
-		r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-			if l := GetLogger(r.Context()); l != nil {
-				l.Info("test log message")
-			}
-			w.WriteHeader(http.StatusOK)
-		})
-		return r
-	}
+        r.Use(New(*cfg.logger, cfg.extractFunc))
+        r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+            if l := GetLogger(r.Context()); l != nil {
+                l.Info("test log message")
+            }
+            w.WriteHeader(http.StatusOK)
+        })
+        return r
+    }
 
-	BeforeAll(func() {
-		for name, cfg := range routerConfigs {
-			routersMap[name] = buildRouter(cfg)
-		}
-	})
+    BeforeAll(func() {
+        for name, cfg := range routerConfigs {
+            routersMap[name] = buildRouter(cfg)
+        }
+    })
 
-	It("should inject a context logger and log a test message for the default router", func() {
-		w := httptest.NewRecorder()
-		req := httptest.NewRequest("GET", "/", nil)
-		req.Header.Set(requestid.RequestARMClientRequestIDHeader, "test-request-id")
+    It("should inject a context logger and log a test message for the default router", func() {
+        w := httptest.NewRecorder()
+        req := httptest.NewRequest("GET", "/", nil)
+        req.Header.Set(requestid.RequestARMClientRequestIDHeader, "test-request-id")
 
-		routersMap[defaultRouterName].ServeHTTP(w, req)
+        routersMap[defaultRouterName].ServeHTTP(w, req)
 
-		out := routerConfigs[defaultRouterName].buf.String()
-		Expect(out).To(ContainSubstring(`"source":"CtxLog"`))
-		Expect(out).To(ContainSubstring(`"method":"GET"`))
-		Expect(out).To(ContainSubstring("test log message"))
-		Expect(w.Result().StatusCode).To(Equal(http.StatusOK))
-	})
+        out := routerConfigs[defaultRouterName].buf.String()
+        Expect(out).To(ContainSubstring(`"source":"CtxLog"`))
+        Expect(out).To(ContainSubstring(`"method":"GET"`))
+        Expect(out).To(ContainSubstring("test log message"))
+        Expect(w.Result().StatusCode).To(Equal(http.StatusOK))
+    })
 
-	It("should log fields from the extraction function when provided", func() {
-		w := httptest.NewRecorder()
-		req := httptest.NewRequest("GET", "/", nil)
-		req.Header.Set(requestid.RequestAcsOperationIDHeader, "test-operation-id")
-		req.Header.Set(requestid.RequestCorrelationIDHeader, "test-correlation-id")
-		req.Header.Set(requestid.RequestARMClientRequestIDHeader, "test-request-id")
+    It("should log fields from the extraction function when provided", func() {
+        w := httptest.NewRecorder()
+        req := httptest.NewRequest("GET", "/", nil)
+        req.Header.Set(requestid.RequestAcsOperationIDHeader, "test-operation-id")
+        req.Header.Set(requestid.RequestCorrelationIDHeader, "test-correlation-id")
+        req.Header.Set(requestid.RequestARMClientRequestIDHeader, "test-request-id")
 
-		routersMap[extraLoggingVariablesName].ServeHTTP(w, req)
+        routersMap[extraLoggingVariablesName].ServeHTTP(w, req)
 
-		out := routerConfigs[extraLoggingVariablesName].buf.String()
-		logInfo, err := unmarshalLog(out)
-		Expect(err).NotTo(HaveOccurred(), "failed to parse log string")
+        out := routerConfigs[extraLoggingVariablesName].buf.String()
+        logInfo, err := unmarshalLog(out)
+        Expect(err).NotTo(HaveOccurred(), "failed to parse log string")
 
-		lines := strings.Split(out, "\n")
-		var headersMap map[string]interface{}
-		for _, line := range lines {
-			if strings.Contains(line, `"headers"`) {
-				headersMap, err = common.UnmarshalHeaders(line)
-				Expect(err).ToNot(HaveOccurred(), "failed to unmarshal headers from log output")
-				break
-			}
-		}
-		Expect(headersMap["operationid"]).To(Equal("test-operation-id"))
-		Expect(headersMap["correlationid"]).To(Equal("test-correlation-id"))
-		// Verify extra extracted attributes appear.
-		Expect(logInfo[resourceGroupNameKey]).To(Equal("extractedRGnamevalue"))
-		Expect(logInfo[subscriptionIDKey]).To(Equal("extractedSubIDvalue"))
-		Expect(logInfo[resultTypeKey]).To(Equal(float64(3)))
-		Expect(logInfo[errorDetailsKey]).To(Equal("extractedErrorDetailsvalue"))
-		Expect(w.Result().StatusCode).To(Equal(http.StatusOK))
-	})
+        lines := strings.Split(out, "\n")
+        var headersMap map[string]interface{}
+        for _, line := range lines {
+            if strings.Contains(line, `"headers"`) {
+                headersMap, err = common.UnmarshalHeaders(line)
+                Expect(err).ToNot(HaveOccurred(), "failed to unmarshal headers from log output")
+                break
+            }
+        }
+        Expect(headersMap["operationid"]).To(Equal("test-operation-id"))
+        Expect(headersMap["correlationid"]).To(Equal("test-correlation-id"))
+        // Verify extra extracted attributes appear.
+        Expect(logInfo[resourceGroupNameKey]).To(Equal("extractedRGnamevalue"))
+        Expect(logInfo[subscriptionIDKey]).To(Equal("extractedSubIDvalue"))
+        Expect(logInfo[resultTypeKey]).To(Equal(float64(3)))
+        Expect(logInfo[errorDetailsKey]).To(Equal("extractedErrorDetailsvalue"))
+        Expect(w.Result().StatusCode).To(Equal(http.StatusOK))
+    })
 
-	It("should continue logging even if an attribute cannot be marshaled", func() {
-		// Capture os.Stdout for temp log message on failed unmarshal
-		oldStdout := os.Stdout
-		r, w, _ := os.Pipe()
-		os.Stdout = w
+    It("should continue logging even if an attribute cannot be marshaled", func() {
+        // Capture os.Stdout for temp log message on failed unmarshal
+        oldStdout := os.Stdout
+        r, w, _ := os.Pipe()
+        os.Stdout = w
 
-		wr := httptest.NewRecorder()
-		req := httptest.NewRequest("GET", "/", nil)
-		req.Header.Set(requestid.RequestAcsOperationIDHeader, "test-operation-id")
-		req.Header.Set(requestid.RequestCorrelationIDHeader, "test-correlation-id")
-		req.Header.Set(requestid.RequestARMClientRequestIDHeader, "test-request-id")
+        wr := httptest.NewRecorder()
+        req := httptest.NewRequest("GET", "/", nil)
+        req.Header.Set(requestid.RequestAcsOperationIDHeader, "test-operation-id")
+        req.Header.Set(requestid.RequestCorrelationIDHeader, "test-correlation-id")
+        req.Header.Set(requestid.RequestARMClientRequestIDHeader, "test-request-id")
 
-		routersMap[extraLoggingCannotMarshal].ServeHTTP(wr, req)
+        routersMap[extraLoggingCannotMarshal].ServeHTTP(wr, req)
 
-		w.Close()
-		os.Stdout = oldStdout
-		var buf bytes.Buffer
-		io.Copy(&buf, r)
+        w.Close()
+        os.Stdout = oldStdout
+        var buf bytes.Buffer
+        io.Copy(&buf, r)
 
-		out := routerConfigs[extraLoggingCannotMarshal].buf.String()
-		Expect(out).To(ContainSubstring(`\"operationid\":\"test-operation-id\"`))
-		Expect(out).To(ContainSubstring(`\"correlationid\":\"test-correlation-id\"`))
+        out := routerConfigs[extraLoggingCannotMarshal].buf.String()
+        fmt.Println("out: ", out)
+        Expect(out).To(ContainSubstring(`"operationid":"test-operation-id"`))
+        Expect(out).To(ContainSubstring(`"correlationid":"test-correlation-id"`))
+        Expect(buf.String()).To(ContainSubstring("error building attributes for additional attributes"))
+        Expect(out).To(ContainSubstring(fmt.Sprintf(`"%s":"!ERROR:json: unsupported type: func()"`, "log")))
+    })
 
-		Expect(buf.String()).To(ContainSubstring("error building attributes for additional attributes"))
-	})
+    It("should include custom static attributes for the custom router", func() {
+        w := httptest.NewRecorder()
+        req := httptest.NewRequest("GET", "/", nil)
 
-	It("should include custom static attributes for the custom router", func() {
-		w := httptest.NewRecorder()
-		req := httptest.NewRequest("GET", "/", nil)
+        routersMap[customTestRouterName].ServeHTTP(w, req)
 
-		routersMap[customTestRouterName].ServeHTTP(w, req)
+        out := routerConfigs[customTestRouterName].buf.String()
+        Expect(out).To(ContainSubstring(`"source":"CtxLog"`))
+        Expect(out).To(ContainSubstring(`"method":"GET"`))
+        Expect(out).To(ContainSubstring(fmt.Sprintf(`"%s":"%s"`, customTestKey, customTestValue)))
+        Expect(w.Result().StatusCode).To(Equal(http.StatusOK))
+    })
 
-		out := routerConfigs[customTestRouterName].buf.String()
-		Expect(out).To(ContainSubstring(`"source":"CtxLog"`))
-		Expect(out).To(ContainSubstring(`"method":"GET"`))
-		Expect(out).To(ContainSubstring(fmt.Sprintf(`"%s":"%s"`, customTestKey, customTestValue)))
-		Expect(w.Result().StatusCode).To(Equal(http.StatusOK))
-	})
+    It("Should be able to retrieve the logger already set in context with GetLogger()", func() {
+        expectedLogger := slog.New(slog.NewTextHandler(io.Discard, nil))
+        ctx := WithLogger(context.Background(), expectedLogger)
+        gotLogger := GetLogger(ctx)
 
-	It("Should be able to retrieve the logger already set in context with GetLogger()", func() {
-		expectedLogger := slog.New(slog.NewTextHandler(io.Discard, nil))
-		ctx := WithLogger(context.Background(), expectedLogger)
-		gotLogger := GetLogger(ctx)
-
-		Expect(gotLogger).To(Equal(expectedLogger), "expected logger from context, got a different instance")
-	})
+        Expect(gotLogger).To(Equal(expectedLogger), "expected logger from context, got a different instance")
+    })
 })
 
 func unmarshalLog(out string) (map[string]interface{}, error) {
-	var outer map[string]interface{}
-	if err := json.Unmarshal([]byte(out), &outer); err != nil {
-		return nil, fmt.Errorf("failed to unmarshal log output: %w", err)
-	}
-	logStr, ok := outer["log"].(string)
-	if !ok {
-		return nil, fmt.Errorf("log key not found or not a string in log output")
-	}
-	var inner map[string]interface{}
-	err := json.Unmarshal([]byte(logStr), &inner)
-	if err != nil {
-		return nil, fmt.Errorf("failed to unmarshal log string: %w", err)
-	}
-	return inner, nil
+    var outer map[string]interface{}
+    if err := json.Unmarshal([]byte(out), &outer); err != nil {
+        return nil, fmt.Errorf("failed to unmarshal log output: %w", err)
+    }
+    logStr, ok := outer["log"].(string)
+    if !ok {
+        return nil, fmt.Errorf("log key not found or not a string in log output")
+    }
+    var inner map[string]interface{}
+    err := json.Unmarshal([]byte(logStr), &inner)
+    if err != nil {
+        return nil, fmt.Errorf("failed to unmarshal log string: %w", err)
+    }
+    return inner, nil
 }
